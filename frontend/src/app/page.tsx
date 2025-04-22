8a10a7e6b79aae25caae20e67e320f4a566ceb1d
ManisriKolli <mk19106n@pace.edu>
2025-04-22 15:18:11 -0400

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -308,7 +308,7 @@ export default function Home() {
         method: "POST",
         headers: {
           "Accept": "application/json",
-          "Authorization": `${process.env.NEXT_PUBLIC_HUGGINGFACE_API_KEY}`,
+          "Authorization": `Bearer ${process.env.NEXT_PUBLIC_HUGGINGFACE_API_KEY}`,
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
